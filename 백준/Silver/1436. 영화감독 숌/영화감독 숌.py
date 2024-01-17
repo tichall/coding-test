dbddaebbac1d1574e054fdae0dd70f1f3d1bca77
Tichall <152502639+tichall@users.noreply.github.com>
2024-01-17 18:28:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 656 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,14 @@
+# 1436번 영화감독 숌
+
+n = int(input())
+tmp = 0
+title = 666
+
+while True :
+    if '666' in str(title) :
+        tmp += 1
+    if tmp == n :
+        break
+    title += 1
+
+print(title)
