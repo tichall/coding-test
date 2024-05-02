e01097f3b25de0cfe67a85d2d7dd2fc18a946472
Tichall <152502639+tichall@users.noreply.github.com>
2024-05-02 10:00:08 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -4,11 +4,12 @@ class Solution {
         if (n == 0 || n == 1) {
             answer = n;
         } else { 
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i <= n / 2 ; i++) {
                 if (n % i == 0) {
                     answer += i;
                 }
             }
+            answer += n;
         }
         return answer;
     }
